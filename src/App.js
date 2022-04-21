4bd4d1c01421967635c80600785d1265d33608a6
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-04-20 23:15:24 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import {BrowserRouter} from "react-router-dom"
 import AppRouter from './components/Routers/AppRouter';
-import {AuthProvider} from './components/Providers/AuthProvider';
+// import {AuthProvider} from './components/Providers/AuthProvider';
 import {library} from '@fortawesome/fontawesome-svg-core';
 import {fas} from "@fortawesome/free-solid-svg-icons";
-import { NewsProvider } from './components/Providers/NewsProvider';
+
 
 library.add(fas);
 
@@ -12,11 +12,11 @@ function App() {
 
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <NewsProvider>
+      {/* <AuthProvider> */}
+      
           <AppRouter />
-        </NewsProvider>
-      </AuthProvider>
+       
+      {/* </AuthProvider> */}
     </BrowserRouter>
   );
 }
