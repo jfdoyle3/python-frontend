48bf1cfbd893e15b62fd5da9a38965fdbe04d030
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-04-21 06:47:56 -0400

    added basic react components

---
+++ src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import Button from '../common/Button';
+import CounterBoxes from '../Counter/CounterBoxes';
+
+const Home = () => {
+
+  // const [count, setCount] = useState(0);
+
+  // const handleClick = () => {
+  //   setCount(count + 1);
+  // } 
+
+  // const handleReduce = () => {
+  //   setCount(count - 1);
+  // }
+
+  return (
+   <div>
+     <h1>HOME!!!!!</h1>
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,48 @@
+//  import React, {useContext, Fragment} from 'react';
+import NavButton from './NavButton';
+// import {AuthContext} from '../Providers/AuthProvider'
+
+const NavBar = (props) => {
+  // const [auth] = useContext(AuthContext)
+
+  return (
+    <nav style={{
+      display:'flex',
+      backgroundColor: "blue",
+      position: "absolute",
+      width: "100%",
+      zIndex: 9999,
+      top: 0,
+      left: 0,
+      flexDirection:'row',
+      height: '75px'
+    }}>
+      
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0em 1.2em'
+      }}>
+        <h1 style={{color: "lightgreen"}}>Python-Frontend</h1>
+      </div>
+        <div style={{
+          flex: 1,
+          marginTop: "0em",
+          display: "flex",
+          flexDirection: "row",
+          borderRadius: "70px 0px 0px 70px",
+          background: "transparent",
+          userSelect: "none",
+          alignItems: 'center',
+          padding: '0em 1.2em',
+          justifyContent: 'flex-end'
+        }}>
+          <NavButton to="/" label="Home" />
+        
+          
+        </div>
+    </nav> 
+  )
+}
+
+export default NavBar;
